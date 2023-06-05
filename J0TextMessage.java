6601eb6f59d3a932da23e7387e99d2bc2a0af108
Zaninhazevedo <92881231+Zaninhazevedo@users.noreply.github.com>
2023-06-05 22:57:28 +0100

    Displaying text message on the screen
    This program displays a text message on the screen.

---
+++ J0TextMessage.java
@@ -0,0 +1,26 @@
+public class J0TextMessage {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+What I have gleaned is as follows:
+
+- Line 1: 
+This line declares a public class named "textmessage". In Java, a class is a blueprint 
+for creating objects. The name of the class must match the name of the file in which it 
+is defined (in this case, "textmessage.java").
+
+- Line 2: 
+This line declares a public static method called "main". In Java, the "main" method 
+serves as the entry point of the program. It is the method that is executed when you 
+run the program. The method takes an array of strings called "args" as a parameter. 
+These arguments can be passed to the program when running it from the command line, 
+but in this case, they are not used.
+
+- Line 3:
+This line uses the "System.out.println()" statement to print the text "Hello World!" to
+the console. The "System.out" refers to the standard output, and "println()" is a 
+method that prints a line of text. In this case, it prints the string "Hello World!".
+*/
